3bb5b0eb3b8fc90c67a9e3ce027a3aa6969a0c47
oliverhnat <oliver.hnat@gmail.com>
2023-12-01 10:18:01 +0100

    Day 1: part 2 done

--- Day1/ans.py
+++ Day1/ans.py
@@ -7,15 +7,36 @@ def parseInput(file):
 
 input = parseInput("input.in").split()
 
-sum = 0
-for line in input:
-    match = re.findall("\\d", line)
-    first = match[0]
-    last = match[-1]
-    if first and last:
-        num = str(first) + str(last)
-        sum += int(num)
-    else:
-        break
+def part1():
+    sum = 0
+    for line in input:
+        match = re.findall("\\d", line)
+        first = match[0]
+        last = match[-1]
+        if first and last:
+            num = str(first) + str(last)
+            sum += int(num)
+        else:
+            break
 
-print(sum)
+    print(sum)
+
+digits = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+def part2():
+    sum = 0
+    for line in input:
+        match = re.findall("(?=(one|two|three|four|five|six|seven|eight|nine|\\d))", line)
+        first = match[0]
+        last = match[-1]
+        if not first.isnumeric():
+            first = digits.index(first)
+        if not last.isnumeric():
+            last = digits.index(last)
+        if first and last:
+            num = f"{first}{last}"
+            sum += int(num)
+        else:
+            print("breaking")
+            break
+    print(sum)
+
