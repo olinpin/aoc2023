149f6cddb5c20359c2f2610b7600a947d9919abf
oliverhnat <oliver.hnat@gmail.com>
2023-12-04 07:32:53 +0100

    Day 4: part 2 done

--- Day4/ans.py
+++ Day4/ans.py
@@ -6,13 +6,13 @@ def parseInput(file):
         return f.read()
 
 
-input = parseInput("input.in").split("\n")
+fileInput = parseInput("input.in").split("\n")
 
 # delete the extra line
-input.pop(-1)
+fileInput.pop(-1)
 
 
-def part1():
+def part1(input):
     totalPoints = 0
     for line in input:
         numbers = line.split("|")
@@ -25,4 +25,29 @@ def part1():
                 points += 1 if points == 0 else points
         totalPoints += points
 
-    print(totalPoints)
+    return totalPoints
+
+
+def part2():
+    cards = {}
+    index = 0
+    cards[index] = 1
+    for line in fileInput:
+        numbers = line.split("|")
+        myNumbers = numbers[0].split(":")[1]
+        myNumbers = re.findall("\\d+", myNumbers)
+        winningNumbers = re.findall("\\d+", numbers[1])
+        same = 0
+        for number in myNumbers:
+            if number in winningNumbers:
+                same += 1
+        for number in range(max(same, 1)):
+            key = number + index + 1  # get the number of the next card
+            if key >= len(fileInput):
+                # break if you're at the end of cards
+                break
+            cards.setdefault(key, 1)
+            if same > 0:
+                cards[key] += cards[index]
+        index += 1
+    print(sum(cards.values()))
