b67ddffafd835c1291253b5a9dc02002ca05dc90
oliverhnat <oliver.hnat@gmail.com>
2023-12-12 15:42:09 +0100

    Day 12: part 2 done

--- Day12/ans.py
+++ Day12/ans.py
@@ -1,5 +1,4 @@
 import re
-from itertools import groupby
 
 
 def parseInput(file):
@@ -7,9 +6,9 @@ def parseInput(file):
         return f.read()
 
 
-def part1():
+def part1(lines):
     total = 0
-    for line in fileInput.splitlines():
+    for line in lines:
         pattern, numbers = line.split(" ")
         numbers = list(map(int, re.findall("\\d+", numbers)))
         pattern = [*pattern]
@@ -18,21 +17,37 @@ def part1():
     print(total)
 
 
+cache = {}
 def rec(pattern, numbers):
-    qm = [i for i, ch in enumerate(pattern) if ch == "?"]
+    key = (pattern, numbers)
+    if key in cache:
+        return cache[key]
+    if pattern == "":
+        return int(numbers == ())
+    if numbers == ():
+        return int("#" not in pattern)
+
     res = 0
-    if len(qm) > 0:
-        i = pattern.index("?")
-        dotPattern = pattern[:]
-        dotPattern[i] = "."
-        res += rec(dotPattern, numbers)
-        htPattern = pattern[:]
-        htPattern[i] = "#"
-        res += rec(htPattern, numbers)
-    else:
-        groups = [sum(1 for _ in gb) for label, gb in groupby(pattern) if label == "#"]
-        return 1 if groups == numbers else 0
+    if pattern[0] in ".?":
+        res += rec(pattern[1:], numbers)
+    if pattern[0] in "#?":
+        if numbers[0] <= len(pattern):
+            if "." not in pattern[:numbers[0]]:
+                if numbers[0] == len(pattern) or pattern[numbers[0]] != "#":
+                    res += rec(pattern[numbers[0]+1:], numbers[1:])
+    cache[key] = res
     return res
+
+
+def part2():
+    total = 0
+    for line in fileInput.splitlines():
+        pattern, numbers = line.split()
+        numbers = tuple(map(int, re.findall("\\d+", numbers)))
+        pattern = "?".join([pattern] * 5)
+        numbers *= 5
+        total += rec(pattern, numbers)
+    print(total)
 
 
 fileInput = parseInput("input.in")
