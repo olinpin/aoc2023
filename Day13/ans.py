fedddae9f3186872621a252293cb6f1311184004
oliverhnat <oliver.hnat@gmail.com>
2023-12-13 12:34:32 +0100

    Day 13: part 1 done

---
+++ Day13/ans.py
@@ -0,0 +1,57 @@
+def parseInput(file):
+    with open(file, "r") as f:
+        return f.read()
+
+
+def part1():
+    patterns = fileInput.split("\n\n")
+    res = 0
+    for pattern in patterns:
+        splitted = pattern.split("\n")
+        rows = findDuplicates(splitted)
+        trans = transpose(pattern)
+        columns = findDuplicates(trans)
+        if rows >= columns:
+            res += 100 * rows
+        else:
+            res += columns
+    print(res)
+
+
+def findDuplicates(splitted):
+    if splitted[-1] == "":
+        splitted.remove("")
+    for index, line in enumerate(splitted):
+        # start at the second line
+        if index == 0:
+            continue
+        if line == splitted[index-1]:
+            possibleMatch = index
+            i = 0
+            while index + i < len(splitted) and (index - i - 1) >= 0:
+                if splitted[index + i] != splitted[index - i - 1]:
+                    break
+                i += 1
+            else:
+                return possibleMatch
+    return 0
+
+
+def transpose(pattern):
+    pattern = pattern.split("\n")
+    newPattern = {}
+    for row in pattern:
+        for i, column in enumerate([*row]):
+            if newPattern.get(i, None) is None:
+                newPattern[i] = []
+            newPattern[i].append(column)
+
+    return list(newPattern.values())
+
+
+def strprint(lines):
+    for line in lines:
+        print("".join(line))
+
+
+fileInput = parseInput("input.in")
