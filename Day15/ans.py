167d354d36a4cbf44f61bbc899d8080043b4fdf2
oliverhnat <oliver.hnat@gmail.com>
2023-12-17 00:09:41 +0100

    Day 15: part 2 done

--- Day15/ans.py
+++ Day15/ans.py
@@ -1,3 +1,6 @@
+import re
+
+
 def parseInput(file):
     with open(file, "r") as f:
         return f.read()
@@ -10,10 +13,46 @@ def part1():
     sequences = fileInput.strip().split(",")
     total = 0
     for sequence in sequences:
-        sequenceTotal = 0
-        for character in [*sequence]:
-            sequenceTotal += ord(character)
-            sequenceTotal *= 17
-            sequenceTotal %= 256
-        total += sequenceTotal
+        total += findHash(sequence)
+    print(total)
+
+
+def findHash(sequence):
+    sequenceTotal = 0
+    for character in [*sequence]:
+        sequenceTotal += ord(character)
+        sequenceTotal *= 17
+        sequenceTotal %= 256
+    return sequenceTotal
+
+
+def part2():
+    boxes = {}
+    sequences = fileInput.strip().split(",")
+    for sequence in sequences:
+        match = re.findall("(\\w+)(=|-)(\\d+)?", sequence)[0]
+        boxNumber = findHash(match[0])
+        if boxNumber not in boxes:
+            boxes[boxNumber] = []
+        if match[1] == "-":
+            for i, box in enumerate(boxes[boxNumber]):
+                label = box[0]
+                if match[0] == label:
+                    boxes[boxNumber].pop(i)
+                    break
+        else:
+            for i, box in enumerate(boxes[boxNumber]):
+                label = box[0]
+                if match[0] == label:
+                    boxes[boxNumber][i] = (match[0], int(match[2]))
+                    break
+            else:
+                boxes[boxNumber].append((match[0], int(match[2])))
+    total = 0
+    for boxNumber in boxes.keys():
+        for i, lens in enumerate(boxes[boxNumber]):
+            lensTotal = 1 + boxNumber
+            lensTotal *= i + 1
+            lensTotal *= lens[1]
+            total += lensTotal
     print(total)
