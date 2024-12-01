7e89a183ca2248a0552412fe1bdf8450050ffdc4
oliverhnat <oliver.hnat@gmail.com>
2024-12-01 10:14:16 +0100

    feat(day1): finish part 2

--- 2024/Day1/ans.py
+++ 2024/Day1/ans.py
@@ -17,3 +17,22 @@ def part1():
     for i in range(len(left)):
         difference += abs(left[i] - right[i])
     print(difference)
+
+def part2():
+    left = []
+    right = []
+    for pair in input:
+        a,b = pair.split()
+        left.append(int(a))
+        right.append(int(b))
+    left.sort()
+    right.sort()
+
+    similarityScore = 0
+    frequency = {}
+    for num in right:
+        f = frequency.get(num, 0) + 1
+        frequency[num] = f
+    for num in left:
+        similarityScore += num * frequency.get(num, 0)
+    print(similarityScore)
