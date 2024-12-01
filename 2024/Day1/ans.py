852b3dc6ac2ac0fb1c11d80e4d6de1f424b6a0a1
oliverhnat <oliver.hnat@gmail.com>
2024-12-01 10:16:53 +0100

    refactor(day1): split into functions, small fixes

--- 2024/Day1/ans.py
+++ 2024/Day1/ans.py
@@ -5,20 +5,24 @@ def parseInput(file):
 input = parseInput("input.in").split("\n")[:-1]
 
 def part1():
-    left = []
-    right = []
-    for pair in input:
-        a,b = pair.split()
-        left.append(int(a))
-        right.append(int(b))
-    left.sort()
-    right.sort()
+    left, right = getBothLists(input)
     difference = 0
     for i in range(len(left)):
         difference += abs(left[i] - right[i])
     print(difference)
 
 def part2():
+    left, right = getBothLists(input)
+    similarityScore = 0
+    frequency = {}
+    for num in right:
+        frequency[num] = frequency.get(num, 0) + 1
+    for num in left:
+        similarityScore += num * frequency.get(num, 0)
+    print(similarityScore)
+
+
+def getBothLists(input):
     left = []
     right = []
     for pair in input:
@@ -27,12 +31,4 @@ def part2():
         right.append(int(b))
     left.sort()
     right.sort()
-
-    similarityScore = 0
-    frequency = {}
-    for num in right:
-        f = frequency.get(num, 0) + 1
-        frequency[num] = f
-    for num in left:
-        similarityScore += num * frequency.get(num, 0)
-    print(similarityScore)
+    return left, right
