a85175e5f0eabd33401ded13f47ed24765978dff
oliverhnat <oliver.hnat@gmail.com>
2024-12-04 09:19:35 +0100

    refactor(day4): better looking code

--- 2024/Day4/ans.py
+++ 2024/Day4/ans.py
@@ -1,64 +1,33 @@
-from typing import List, Callable, Tuple
+from typing import List, Callable, Optional, Tuple
+
+
 def parseInput(file):
     with open(file, "r") as f:
         return f.read()
 
+
 input = parseInput("input.in").split("\n")[:-1]
+
 
 def part1():
     matrix = [list(i) for i in input]
     found = 0
+    directions = [
+        down,
+        up,
+        left,
+        right,
+        topRight,
+        topLeft,
+        bottomRight,
+        bottomLeft,
+    ]
     for x in range(len(matrix)):
         for y in range(len(matrix[x])):
-            found += int(search(down, x, y, matrix))
-            found += int(search(up, x, y, matrix))
-            found += int(search(left, x, y, matrix))
-            found += int(search(right, x, y, matrix))
-            found += int(search(topRight, x, y, matrix))
-            found += int(search(topLeft, x, y, matrix))
-            found += int(search(bottomRight, x, y, matrix))
-            found += int(search(bottomLeft, x, y, matrix))
+            for direction in directions:
+                found += int(search(direction, x, y, matrix))
     print(found)
 
-def search(updateCoords: Callable[[int, int], Tuple[int,int]], x: int, y: int, matrix: List[List[str]]) -> bool:
-    if checkCoord("X", x, y, matrix):
-        x, y = updateCoords(x, y)
-        if checkCoord("M", x, y, matrix):
-            x, y = updateCoords(x, y)
-            if checkCoord("A", x, y, matrix):
-                x, y = updateCoords(x, y)
-                return checkCoord("S", x, y, matrix)
-    return False
-
-def down(x: int, y: int) -> Tuple[int, int]:
-    return (x+1, y)
-
-def up(x: int, y: int) -> Tuple[int, int]:
-    return (x-1, y)
-
-def left(x: int, y: int) -> Tuple[int, int]:
-    return (x, y-1)
-
-def right(x: int, y: int) -> Tuple[int, int]:
-    return (x, y+1)
-
-def topRight(x: int, y: int) -> Tuple[int, int]:
-    return (x-1, y+1)
-
-def topLeft(x: int, y: int) -> Tuple[int, int]:
-    return (x-1, y-1)
-
-def bottomRight(x: int, y: int) -> Tuple[int, int]:
-    return (x+1, y+1)
-
-def bottomLeft(x: int, y: int) -> Tuple[int, int]:
-    return (x+1, y-1)
-
-def checkCoord(letter: str, x: int, y: int, matrix: List[List[str]]) -> bool:
-    return validCoordinates(x, y, matrix) and matrix[x][y] == letter
-
-def validCoordinates(x: int, y: int, matrix: List[List[str]]) -> bool:
-    return x >= 0 and x < len(matrix) and y >= 0 and y < len(matrix[x])
 
 def part2():
     matrix = [list(i) for i in input]
@@ -68,18 +37,78 @@ def part2():
             found += int(search2(x, y, matrix))
     print(found)
 
-def search2(x: int, y: int, matrix: List[List[str]]) -> bool:
-    originalX, originalY = x, y
-    cross = 0
-    if checkCoord("A", x, y, matrix):
-        tLeftX, tLeftY = topLeft(originalX, originalY)
-        tRightX, tRightY = topRight(originalX, originalY)
-        bLeftX, bLeftY = bottomLeft(originalX, originalY)
-        bRightX, bRightY = bottomRight(originalX, originalY)
-        cross += checkCoord("M", tLeftX, tLeftY, matrix) and checkCoord("S", bRightX, bRightY, matrix)
-        cross += checkCoord("S", tLeftX, tLeftY, matrix) and checkCoord("M", bRightX, bRightY, matrix)
-        cross += checkCoord("M", tRightX, tRightY, matrix) and checkCoord("S", bLeftX, bLeftY, matrix)
-        cross += checkCoord("S", tRightX, tRightY, matrix) and checkCoord("M", bLeftX, bLeftY, matrix)
-    return cross == 2
 
-part2()
+def search(
+    updateCoords: Callable[[int, int], Tuple[int, int]],
+    x: int,
+    y: int,
+    matrix: List[List[str]],
+) -> bool:
+    if checkCoord("X", x, y, matrix):
+        x, y = updateCoords(x, y)
+        if checkCoord("M", x, y, matrix):
+            x, y = updateCoords(x, y)
+            if checkCoord("A", x, y, matrix):
+                x, y = updateCoords(x, y)
+                return checkCoord("S", x, y, matrix)
+    return False
+
+
+def search2(x: int, y: int, matrix: List[List[str]]) -> bool:
+    if checkCoord("A", x, y, matrix):
+        tLeftX, tLeftY = topLeft(x, y)
+        tRightX, tRightY = topRight(x, y)
+        bLeftX, bLeftY = bottomLeft(x, y)
+        bRightX, bRightY = bottomRight(x, y)
+        cross1 = set(
+            [getCoord(tLeftX, tLeftY, matrix), getCoord(bRightX, bRightY, matrix)]
+        )
+        cross2 = set(
+            [getCoord(tRightX, tRightY, matrix), getCoord(bLeftX, bLeftY, matrix)]
+        )
+        return cross1 == set(["S", "M"]) and cross1 == cross2
+    return False
+
+
+def down(x: int, y: int) -> Tuple[int, int]:
+    return (x + 1, y)
+
+
+def up(x: int, y: int) -> Tuple[int, int]:
+    return (x - 1, y)
+
+
+def left(x: int, y: int) -> Tuple[int, int]:
+    return (x, y - 1)
+
+
+def right(x: int, y: int) -> Tuple[int, int]:
+    return (x, y + 1)
+
+
+def topRight(x: int, y: int) -> Tuple[int, int]:
+    return (x - 1, y + 1)
+
+
+def topLeft(x: int, y: int) -> Tuple[int, int]:
+    return (x - 1, y - 1)
+
+
+def bottomRight(x: int, y: int) -> Tuple[int, int]:
+    return (x + 1, y + 1)
+
+
+def bottomLeft(x: int, y: int) -> Tuple[int, int]:
+    return (x + 1, y - 1)
+
+
+def checkCoord(letter: str, x: int, y: int, matrix: List[List[str]]) -> bool:
+    return validCoordinates(x, y, matrix) and matrix[x][y] == letter
+
+
+def getCoord(x: int, y: int, matrix: List[List[str]]) -> Optional[str]:
+    return matrix[x][y] if validCoordinates(x, y, matrix) else None
+
+
+def validCoordinates(x: int, y: int, matrix: List[List[str]]) -> bool:
+    return x >= 0 and x < len(matrix) and y >= 0 and y < len(matrix[x])
