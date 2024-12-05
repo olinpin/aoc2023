7ecaf695c5522c40ae358b89a64466a33c429a06
oliverhnat <oliver.hnat@gmail.com>
2024-12-05 10:47:42 +0100

    feat(day5): part 2 + refactor

--- 2024/Day5/ans.py
+++ 2024/Day5/ans.py
@@ -8,10 +8,43 @@ def parseInput(file):
 input = parseInput("input.in").split("\n")[:-1]
 
 def part1():
-    print(input)
+    allOrdering, _, _ = getOrderingAndRules(input)
+    sum = 0
+    for order in allOrdering:
+        mid = int((len(order) - 1) / 2)
+        sum += order[mid]
+    print(sum)
+
+
+def part2():
+    _, rules, unOrdered = getOrderingAndRules(input)
+    i = 0
+    while i < len(unOrdered):
+        ordering = unOrdered[i]
+        previous = []
+        for j in range(len(ordering)):
+            intersection =  [x for x in rules.get(int(ordering[j]), set([])) if x in previous]
+            if intersection != []:
+                newI = ordering.index(intersection[0])
+                value = ordering.pop(j)
+                ordering.insert(newI, value)
+                break
+            previous.append(ordering[j])
+        else:
+            unOrdered[i] = previous
+            i += 1
+    sum = 0
+    for order in unOrdered:
+        mid = int((len(order) - 1) / 2)
+        sum += order[mid]
+    print(sum)
+
+
+def getOrderingAndRules(input):
     rules = {}
     ordering = False
     allOrdering = []
+    unOrdered = []
     for i in input:
         if i == '':
             ordering = True
@@ -26,11 +59,8 @@ def part1():
                 orders.append(int(pageNumber))
                 intersection =  [x for x in rules.get(int(pageNumber), set([])) if x in orders]
                 if intersection != []:
+                    unOrdered.append([int(x) for x in i.split(',')])
                     break
             else:
                 allOrdering.append(orders)
-    sum = 0
-    for order in allOrdering:
-        mid = int((len(order) - 1) / 2)
-        sum += order[mid]
-    print(sum)
+    return allOrdering, rules, unOrdered
